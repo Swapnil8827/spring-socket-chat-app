ae2e431adbc29ae31b458fdf4223225640c1db11
Swapnil8827 <57755983+Swapnil8827@users.noreply.github.com>
2022-05-30 16:54:27 +0530

    Add files via upload

---
+++ src/main/java/com/socket/test/SpringBootSocketTestApplication.java
@@ -0,0 +1,13 @@
+package com.socket.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocketTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSocketTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socket/test/SpringBootSocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socket.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSocketTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
